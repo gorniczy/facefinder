b1ff1598dd4ae66d679bbd60804b9fc32dd84ff3
gorniczy <denysgornichar@gmail.com>
2019-04-28 22:00:18 +0200

    add Navigation component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Navigation from "./components/navigation/Navigation";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      // <Logo />
-      // <LinkForm />
-      // <Image />
+      {
+        // <Logo />
+        // <LinkForm />
+        // <Image />
+      }
     </div>
   );
 }
