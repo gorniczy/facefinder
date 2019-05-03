412c042ae8ee7e8137cab7289273c183191836ed
gorniczy <denysgornichar@gmail.com>
2019-05-03 21:19:30 +0200

    add particles package and use it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
-import Navigation from "./components/navigation/Navigation";
-import Logo from "./components/logo/Logo";
-import Rank from "./components/rank/Rank";
-import LinkForm from "./components/linkform/LinkForm";
-import "./App.css";
+import React from "react"
+import Navigation from "./components/navigation/Navigation"
+import Logo from "./components/logo/Logo"
+import Rank from "./components/rank/Rank"
+import LinkForm from "./components/linkform/LinkForm"
+import Particles from "react-particles-js"
+import "./App.css"
+
+const particlesParams = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 600
+      }
+    },
+    color: {
+      value: "#227064"
+    },
+    line_linked: {
+      color: "#227064"
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particlesParams} />
       <Navigation />
       <Logo />
       <Rank />
@@ -16,7 +36,7 @@ function App() {
         // <Image />
       }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
