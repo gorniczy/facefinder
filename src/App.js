5d3edb08145c2a68fb710607f0504925f42a08a8
gorniczy <denysgornichar@gmail.com>
2019-05-03 20:50:16 +0200

    add Rank component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navigation from "./components/navigation/Navigation";
 import Logo from "./components/logo/Logo";
+import Rank from "./components/rank/Rank";
 import LinkForm from "./components/linkform/LinkForm";
 import "./App.css";
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
       <LinkForm />
       {
         // <Image />
---
+++ src/components/rank/Rank.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./Rank.css"
+
+const Rank = () => {
+  return (
+    <div>
+      <p className="white f3">{"Śli, your current rank is "}</p>
+      <p className="white f1">{"5"}</p>
+    </div>
+  )
+}
+
+export default Rank
