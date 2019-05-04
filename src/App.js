3fad036680adc5199cf49953be90aa52b0b85aff
gorniczy <denysgornichar@gmail.com>
2019-05-04 18:21:59 +0200

    add onChange and onSubmit functions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Logo from "./components/logo/Logo"
 import Rank from "./components/rank/Rank"
 import LinkForm from "./components/linkform/LinkForm"
 import Particles from "react-particles-js"
+import Clarifai from "clarifai"
 import "./App.css"
 
 const particlesParams = {
@@ -24,19 +25,50 @@ const particlesParams = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className="particles" params={particlesParams} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <LinkForm />
-      {
-        // <Image />
-      }
-    </div>
-  )
+const app = new Clarifai.App({ apiKey: "f0adc6d475564f23bd76e5847bc1f78a" })
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      input: ""
+    }
+  }
+
+  onChange = event => {
+    this.setState = {
+      input: event.target.value
+    }
+  }
+
+  onSubmit = () => {
+    app.models
+      .predict(
+        Clarifai.GENERAL_MODEL,
+        "https://samples.clarifai.com/metro-north.jpg"
+      )
+      .then(response => {
+        console.log(response)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesParams} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <LinkForm onChange={this.onChange} onSubmit={this.onSubmit} />
+        {
+          // <Image />
+        }
+      </div>
+    )
+  }
 }
 
 export default App
