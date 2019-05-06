e07286ab13d3e18ab8dd527b9d3718011d356fc0
gorniczy <denysgornichar@gmail.com>
2019-05-06 22:52:06 +0200

    use Clarifai API to frame the picture

--- src/components/image/Image.js
+++ src/components/image/Image.js
@@ -1,15 +1,18 @@
 import React from "react"
+import "./Image.css"
 
 const Image = props => {
   return (
     <div className="center">
-      <div className="pa5">
+      <div className="absolute pa3">
         <img
+          id="linkedInmage"
           src={props.imageURL}
           alt={"processed file"}
           width="500px"
           height="auto"
         />
+        <div className="bounding-box" style={{top: props.box.topRow, right: props.box.rightCol, bottom: props.box.bottomRow, left: props.box.leftCol}}></div>
       </div>
     </div>
   )
